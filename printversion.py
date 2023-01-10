ac8802478355d6849aa065eafbaed6cb7bb80252
dadaqi <77340349+dadaqi@users.noreply.github.com>
2023-01-09 22:14:50 -0700

    change url

--- printversion.py
+++ printversion.py
@@ -1,7 +1,7 @@
 import requests
 
 # Replace USERNAME and REPOSITORY with your GitHub username and repository name
-url = "https://raw.githubusercontent.com/dadaqi/CMPUT404/main/printversion.py?token=GHSAT0AAAAAAB5FMOI6VIFFF5BF53AGXDXSY546PYA"
+url = "https://api.github.com/repos/dadaqi/CMPUT404/contents/printversion.py"
 
 # Send an HTTP GET request to the URL
 response = requests.get(url)
