22d22a8e6906deabc4f85184eb16b5eb4bf92a9f
dadaqi <77340349+dadaqi@users.noreply.github.com>
2023-01-11 14:48:45 -0700

    f

--- printversion.py
+++ printversion.py
@@ -1,19 +1,30 @@
+# import requests
+
+# # Replace USERNAME and REPOSITORY with your GitHub username and repository name
+# url = "https://api.github.com/repos/dadaqi/CMPUT404/contents/printversion.py"
+
+# # Send an HTTP GET request to the URL
+# response = requests.get(url)
+
+# # Check the status code of the response
+# if response.status_code == 200:
+#     script_text = response.text
+#     # Print the source code
+#     print(script_text)
+# else:
+#     # If the request is not successful, print an error message
+#     print("Failed to retrieve source code")
+
 import requests
 
-# Replace USERNAME and REPOSITORY with your GitHub username and repository name
-url = "https://api.github.com/repos/dadaqi/CMPUT404/contents/printversion.py"
+script_url = "https://api.github.com/repos/dadaqi/CMPUT404/contents/printversion.py"
+response = requests.get(script_url)
 
-# Send an HTTP GET request to the URL
-response = requests.get(url)
-
-# Check the status code of the response
 if response.status_code == 200:
     script_text = response.text
-    # Print the source code
     print(script_text)
-else:
-    # If the request is not successful, print an error message
-    print("Failed to retrieve source code")
+    with open("script.py", "w") as file:
+        file.write(script_text)
 
 
 import requests
